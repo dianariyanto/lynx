50a3bb08cac912b219c5b8eebbd6f5d676a5b5d7
riyuejz <50128745+riyuejz@users.noreply.github.com>
2022-01-02 18:36:00 +0700

    Update 30-sysinfo.sh

--- amlogic-s9xxx/common-files/patches/cpustat/30-sysinfo.sh
+++ amlogic-s9xxx/common-files/patches/cpustat/30-sysinfo.sh
@@ -156,34 +156,39 @@ sys_tempx=$(echo $sys_temp | sed 's/ / /g')
 # display info
 
 machine_model=$(cat /proc/device-tree/model | tr -d "\000")
-echo -e " Device Model	: \033[93m${machine_model}\033[0m"
-printf " Architecture	: \x1B[93m%s\x1B[0m" "$sys_tempx"
+# echo -e "  Device Model	: \033[93m${machine_model}\033[0m"
+printf "  Architecture 	: \x1B[93m%s\x1B[0m" "$sys_tempx"
 echo ""
-display " Load Average	" "${load%% *}" "${critical_load}" "0" "" "${load#* }"
+display "  Load Average	" "${load%% *}" "${critical_load}" "0" "" "${load#* }"
 echo ""
-printf " Uptime		: \x1B[92m%s\x1B[0m\t\t" "$time"
+printf "  Uptime	: \x1B[92m%s\x1B[0m\t\t" "$time"
 echo ""
 
-display " Ambient Temp	" "$cpu_tempx" "60" "0" "°C"
+display "  Ambient Temp	" "$cpu_tempx" "60" "0" "°C"
 if [ -x /usr/bin/cpustat ]; then
 	cpu_freq=$(/usr/bin/cpustat -F1500)
 echo ""
-	echo -n " Frequency	: $cpu_freq"
+	echo -n "  Frequency	: $cpu_freq"
 else
 	display "Frequency" "$cpu_freq" "1500" "0" " Mhz" ""
 fi
 echo ""
 
-display " Memory Usage	" "$memory_usage" "70" "0" "%" " of ${memory_total}MB"
+display "  Memory Usage	" "$memory_usage" "70" "0" "%" " of ${memory_total}MB"
 echo ""
-printf " IP Address	: \x1B[92m%s\x1B[0m" "$ip_address"
+printf "[0;34m│[0m IP Address	: \x1B[92m%s\x1B[0m" "$ip_address"
 #display "Swap Usage" "$swap_usage" "10" "0" "%" " of $swap_total""Mb"
 echo ""
 
 #display " Boot Storage	" "$boot_usage" "90" "1" "%" " of $boot_total"
 #echo ""
-display " SYS Storage	" "$root_usage" "90" "1" "%" " of $root_total"
+display "[0;34m│[0m SYS Storage	" "$root_usage" "90" "1" "%" " of $root_total"
 echo ""
+
+if [ "$hdd_usage" != "" ]; then
+	display "[0;34m│[0m Ext. Storage	" "$hdd_usage" "90" "1" "%" " of $hdd_total"
+	echo ""
+fi
 
 #if [ "$data_usage" != "" ]; then
 #	display " Data Storage" "$data_usage" "90" "1" "%" " of $data_total"
@@ -195,8 +200,4 @@ echo ""
 #fi
 #echo ""
 
-if [ "$hdd_usage" != "" ]; then
-	display " Ext. Storage	" "$hdd_usage" "90" "1" "%" " of $hdd_total"
-	echo ""
-fi
-echo " -----------------------------------------------------"
+echo "[0;34m└───────[0;34m─────[0;37m─────────────[0;1;34;94m───────────[0;1;30;90m─────────[0m"
